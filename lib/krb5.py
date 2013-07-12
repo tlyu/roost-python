5fe62e47761665dfe400d53707bfcd51d51d78c8
David Benjamin <davidben@mit.edu>
2013-07-12 19:52:58 -0400

    Add a nicer wrapper over krb5_ctypes
    Fiiine. I guess I'm in the business of writing a python-krb5 now.

---
+++ lib/krb5.py
@@ -0,0 +1,82 @@
+import ctypes
+import functools
+
+import krb5_ctypes
+
+__all__ = ['Context']
+
+class Error(Exception):
+    def __init__(self, ctx_raw, code):
+        self.code = code
+        msg_c = krb5_ctypes.krb5_get_error_message(ctx_raw, code)
+        self.message = msg_c.value
+        krb5_ctypes.krb5_free_error_message(ctx_raw, msg_c)
+
+    def __str__(self):
+        return self.message
+
+def check_error(fn):
+    if fn.restype is not krb5_ctypes.krb5_error_code:
+        return fn
+    @functools.wraps(fn)
+    def wrapped(ctx, *args):
+        ret = fn(ctx, *args)
+        if ret:
+            raise Error(ctx, ret)
+        return ret
+    return wrapped
+
+krb5_init_context = check_error(krb5_ctypes.krb5_init_context)
+krb5_free_context = check_error(krb5_ctypes.krb5_free_context)
+krb5_cc_default = check_error(krb5_ctypes.krb5_cc_default)
+krb5_cc_close = check_error(krb5_ctypes.krb5_cc_close)
+krb5_cc_get_principal = check_error(krb5_ctypes.krb5_cc_get_principal)
+krb5_free_principal = check_error(krb5_ctypes.krb5_free_principal)
+krb5_unparse_name = check_error(krb5_ctypes.krb5_unparse_name)
+krb5_free_unparsed_name = check_error(krb5_ctypes.krb5_free_unparsed_name)
+
+class Context(object):
+    def __init__(self):
+        self._handle = krb5_ctypes.krb5_context()
+        krb5_init_context(self._handle)
+
+    def __del__(self):
+        if bool(self._handle):
+            krb5_free_context(self._handle)
+
+    def cc_default(self):
+        ccache = CCache(self)
+        krb5_cc_default(self._handle, ccache._handle)
+        return ccache
+
+class CCache(object):
+    def __init__(self, ctx):
+        self._ctx = ctx
+        self._handle = krb5_ctypes.krb5_ccache()
+
+    def __del__(self):
+        if bool(self._handle):
+            krb5_cc_close(self._ctx._handle, self._handle)
+
+    def get_principal(self):
+        principal = Principal(self._ctx)
+        krb5_cc_get_principal(self._ctx._handle,
+                              self._handle,
+                              principal._handle)
+        return principal
+
+class Principal(object):
+    def __init__(self, ctx):
+        self._ctx = ctx
+        self._handle = krb5_ctypes.krb5_principal()
+
+    def __del__(self):
+        if bool(self._handle):
+            krb5_free_principal(self._ctx._handle, self._handle)
+
+    def unparse_name(self):
+        name_c = ctypes.c_char_p()
+        krb5_unparse_name(self._ctx._handle, self._handle, name_c)
+        name = name_c.value
+        krb5_free_unparsed_name(self._ctx._handle, name_c)
+        return name
